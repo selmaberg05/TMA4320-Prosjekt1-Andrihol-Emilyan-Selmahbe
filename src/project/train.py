89ed95f9f71f7ba8a6e8dc90f114e23790b4568d
andrihol <andrihol@stud.ntnu.no>
2026-02-04 12:11:41 +0100

    Add objective function for training NN and PINN

--- src/project/train.py
+++ src/project/train.py
@@ -36,6 +36,24 @@ def train_nn(
     #######################################################################
 
     # Update the nn_params and losses dictionary
+    def objective_fn(nn_params):
+        L_data = data_loss(nn_params, sensor_data, cfg)
+        L_ic = ic_loss(nn_params, ic_epoch, cfg)
+        total_loss = cfg.lambda_data * L_data + cfg.lambda_ic * L_ic
+        return total_loss, (L_data, L_ic)
+    
+    value_and_grad = jax.value_and_grad(objective_fn, has_aux=True) 
+
+    from tqdm import tqdm
+    for _ in tqdm(range(cfg.num_epochs), desc="Training NN"):
+        ic_epoch, key = sample_ic(key, cfg)
+        (total_loss, (L_data, L_ic)), grads = value_and_grad(nn_params) 
+
+        nn_params, adam_state = adam_step(nn_params, grads, adam_state, lr=cfg.learning_rate)
+
+        losses["total"].append(total_loss)
+        losses["data"].append(L_data)
+        losses["ic"].append(L_ic)
 
     #######################################################################
     # Oppgave 4.3: Slutt
@@ -66,6 +84,31 @@ def train_pinn(sensor_data: jnp.ndarray, cfg: Config) -> tuple[dict, dict]:
     #######################################################################
 
     # Update the nn_params and losses dictionary
+    def objective_fn(pinn_params, interior_epoch, ic_epoch, bc_epoch):
+        nn_params = pinn_params["nn"]
+        L_data = data_loss(nn_params, sensor_data, cfg)
+        L_physics = physics_loss(pinn_params, interior_epoch, cfg)
+        L_ic = ic_loss(nn_params, ic_epoch, cfg)
+        L_bc = bc_loss(pinn_params,bc_epoch,cfg)
+        total_loss = cfg.lambda_data * L_data + cfg.lambda_physics * L_physics +cfg.lambda_ic * L_ic +cfg.lambda_bc * L_bc
+        return total_loss, (L_data,L_physics, L_ic, L_bc)
+    
+    value_and_grad = jax.jit(jax.value_and_grad(objective_fn, has_aux=True))
+
+    from tqdm import tqdm
+    for _ in tqdm(range(cfg.num_epochs), desc="Training PINN"):
+        interior_epoch, key = sample_interior(key, cfg)
+        ic_epoch, key = sample_ic(key, cfg)
+        bc_epoch, key = sample_bc(key, cfg)
+        (total_loss, (L_data, L_physics, L_ic, L_bc)), grads = value_and_grad(pinn_params,interior_epoch, ic_epoch, bc_epoch) 
+
+        pinn_params, opt_state = adam_step(pinn_params, grads, opt_state, lr=cfg.learning_rate)
+
+        losses["total"].append(total_loss)
+        losses["data"].append(L_data)
+        losses["physics"].append(L_physics)
+        losses["ic"].append(L_ic)
+        losses["bc"].append(L_bc)
 
     #######################################################################
     # Oppgave 5.3: Slutt
